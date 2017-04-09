01244136c2946552e1c071f99fa50801ad2f932b
TheShalit <shalevshalit@gmail.com>
2017-04-09 12:58:07 +0300

    moved data stuctures to DataStructure and Container

--- src/Container.java
+++ src/Container.java
@@ -1,11 +1,77 @@
-
 //Don't change the class name
-public class Container{
-	private Point data;//Don't delete or change this field;
-	
-	//Don't delete or change this function
-	public Point getData()
-	{
-		return data;
-	}
+public class Container {
+    private Point data;//Don't delete or change this field;
+    private Container nextX;
+    private Container prevX;
+    private Container nextY;
+    private Container prevY;
+
+    public Container(Point point) {
+        data = point;
+        nextX = null;
+        nextY = null;
+        prevX = null;
+        prevY = null;
+    }
+
+    public Container(Point point, Container next, Container prev, boolean axis) {
+        data = point;
+        if (axis) {
+            nextX = next;
+            nextY = null;
+            prevX = prev;
+            prevY = null;
+        } else {
+            nextX = null;
+            nextY = next;
+            prevX = null;
+            prevY = prev;
+        }
+    }
+
+    //Don't delete or change this function
+    public Point getData() {
+        return data;
+    }
+
+    public int getPointValue(boolean axis) {
+        if (axis)
+            return data.getX();
+        else
+            return data.getY();
+    }
+
+    public void setLinkNext(Container next, boolean axis) {
+        if (axis)
+            nextX = next;
+        else
+            nextY = next;
+    }
+    /* Function to set link to previous node */
+
+    public void setLinkPrev(Container prev, boolean axis) {
+        if (axis)
+            prevX = prev;
+        else
+            prevY = prev;
+    }
+
+    public Container getLinkNext(boolean axis) {
+        if (axis)
+            return nextX;
+        else
+            return nextY;
+    }
+    /* Function to set link to previous node */
+
+    public Container getLinkPrev(boolean axis) {
+        if (axis)
+            return prevX;
+        else
+            return prevY;
+    }
+
+    public String toString() {
+        return data.toString();
+    }
 }
--- src/DataStructure.java
+++ src/DataStructure.java
@@ -1,70 +1,205 @@
 
 public class DataStructure implements DT {
+    private Container firstByX;
+    private Container lastByX;
+    private Container firstByY;
+    private Container lastByY;
+    int size;
+
+    //////////////// DON'T DELETE THIS CONSTRUCTOR ////////////////
+    public DataStructure() {
+        size = 0;
+        firstByX = null;
+        lastByX = null;
+        firstByY = null;
+        lastByY = null;
+    }
+
+    public Container getFirstByAxis(boolean axis) {
+        if (axis)
+            return firstByX;
+        else
+            return firstByY;
+    }
+
+    public Container getLastByAxis(boolean axis) {
+        if (axis)
+            return lastByX;
+        else
+            return lastByY;
+    }
+
+    public void addPoint(Point point) {
+        Container newPoint = new Container(point);
+        Container curr, next;
+        boolean ins = false;
+        if (isEmpty()) {
+            firstByX = newPoint;
+            lastByX = newPoint;
+            firstByY = newPoint;
+            lastByY = newPoint;
+        } else {
+            //insert to x axis
+
+            if (point.getX() <= firstByX.getData().getX()) {
+                newPoint.setLinkNext(firstByX, true);
+                firstByX.setLinkPrev(newPoint, true);
+                firstByX = newPoint;
+            } else {
+                curr = firstByX;
+                next = curr.getLinkNext(true);
+                int val = newPoint.getData().getX();
+
+                while (next != null) {
+                    if (val >= curr.getData().getX() && val <= next.getData().getX()) {
+                        curr.setLinkNext(newPoint, true);
+                        newPoint.setLinkPrev(curr, true);
+                        newPoint.setLinkNext(next, true);
+                        next.setLinkPrev(newPoint, true);
+                        ins = true;
+                        break;
+                    } else {
+                        curr = next;
+                        next = next.getLinkNext(true);
+                    }
+                }
+                if (!ins) {
+                    curr.setLinkNext(newPoint, true);
+                    newPoint.setLinkPrev(curr, true);
+                    lastByX = newPoint;
+
+                }
+            }//small else
+
+            //insert to y axis
+
+            if (point.getY() <= firstByY.getData().getY()) {
+                newPoint.setLinkNext(firstByY, false);
+                firstByY.setLinkPrev(newPoint, false);
+                firstByY = newPoint;
+            } else {
+                curr = firstByX;
+                next = curr.getLinkNext(false);
+                int val = newPoint.getData().getY();
+
+                while (next != null) {
+                    if (val >= curr.getData().getY() && val <= next.getData().getY()) {
+                        curr.setLinkNext(newPoint, false);
+                        newPoint.setLinkPrev(curr, false);
+                        newPoint.setLinkNext(next, false);
+                        next.setLinkPrev(newPoint, false);
+                        ins = true;
+                        break;
+                    } else {
+                        curr = next;
+                        next = next.getLinkNext(false);
+                    }
+                }
+                if (!ins) {
+                    curr.setLinkNext(newPoint, false);
+                    newPoint.setLinkPrev(curr, false);
+                    lastByY = newPoint;
+
+                }
+            }//small else
+
+        } //notEmpty else
+
+        size++;
+    }
+
+    public boolean isEmpty() {
+        return firstByX.equals(null);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    private int countPointsInRange(int min, int max, boolean axis) {
+
+        int sum = 0;
+        Container curr = getFirstByAxis(axis);
+
+        while (curr != null && curr.getPointValue(axis) <= max & curr.getPointValue(axis) >= min) {
+
+            sum++;
+            curr = curr.getLinkNext(axis);
+        }
+
+        return sum;
+    }
+
+    public Point[] getPointsInRangeRegAxis(int min, int max, Boolean axis) {
+
+        Point[] output = new Point[countPointsInRange(min, max, axis)];
+        Container curr = getFirstByAxis(axis);
+        int index = 0;
+        while (curr != null && curr.getPointValue(axis) >= min & curr.getPointValue(axis) <= max) {
+            output[index] = curr.getData();
+            index++;
+            curr = curr.getLinkNext(axis);
+
+        }
+
+        return output;
+    }
+
+    @Override
+    public Point[] getPointsInRangeOppAxis(int min, int max, Boolean axis) {
+        Point[] output = new Point[countPointsInRange(min, max, axis)];
+        Container curr = getFirstByAxis(!axis);
+
+        int index = 0;
+        while (curr != null && curr.getPointValue(axis) <= max & curr.getPointValue(axis) >= min) {
+
+            output[index] = curr.getData();
+            index++;
+            curr = curr.getLinkNext(!axis);
+        }
+
+        return output;
+    }
+
+    @Override
+    public double getDensity() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void narrowRange(int min, int max, Boolean axis) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Boolean getLargestAxis() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Container getMedian(Boolean axis) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Point[] nearestPairInStrip(Container container, double width,
+                                      Boolean axis) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Point[] nearestPair() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 
-	//////////////// DON'T DELETE THIS CONSTRUCTOR ////////////////
-	public DataStructure()
-	{
+    //TODO: add members, methods, etc.
 
-	}
-
-	@Override
-	public void addPoint(Point point) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Point[] getPointsInRangeRegAxis(int min, int max, Boolean axis) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Point[] getPointsInRangeOppAxis(int min, int max, Boolean axis) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public double getDensity() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void narrowRange(int min, int max, Boolean axis) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Boolean getLargestAxis() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Container getMedian(Boolean axis) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Point[] nearestPairInStrip(Container container, int width,
-			Boolean axis) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Point[] nearestPair() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	
-	//TODO: add members, methods, etc.
-	
 }
 
